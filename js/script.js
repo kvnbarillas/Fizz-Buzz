d1bb14285b955a633ee860cfab9d13bc55ee0a54
Kevin Barillas <kvnbarillas@yahoo.com>
2016-09-22 19:00:54 -0700

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,20 +1,36 @@
-$(document).ready(function() {
-for (i=1; i<=100; i++) {
+var addFizzBuzz = function() {
+	for (i=1; i<=""; i++) {
 
-if (i % 15 == 0) {
-	$("#fizzbuzz").append("<p>FizzBuzz</p>");
-}
+		if (i % 15 == 0) {
+		$("#fizzbuzz").append("<p>FizzBuzz</p>");
+		}
 
-else if (i % 3 == 0) { 
-	$("#fizzbuzz").append("<p>Fizz</p>");
-}
+		else if (i % 3 == 0) { 
+		$("#fizzbuzz").append("<p>Fizz</p>");
+		}
 
-else if (i % 5 == 0){
-	$("#fizzbuzz").append("<p>Buzz</p>");
-}
+		else if (i % 5 == 0){
+		$("#fizzbuzz").append("<p>Buzz</p>");
+		}
 
-else {
-	$("#fizzbuzz").append("<p>"+ i +"</p>");
+		else {
+		$("#fizzbuzz").append("<p>"+ i +"</p>");
+		}
 	}
 }
-})
+
+$(document).ready(function() {
+			var number = prompt("Please Enter Number ");
+			 if(isNaN(number)){
+			 	alert("You must enter a number!");
+			 }
+			else{
+				addFizzBuzz();
+			 }
+});
+
+
+
+
+
+
