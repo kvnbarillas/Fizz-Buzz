323cf3b4e6defbf9b2a2736d264fa1c3257826ac
Kevin Barillas <kvnbarillas@yahoo.com>
2016-09-22 19:30:39 -0700

    Refactor

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-var addFizzBuzz = function() {
-	for (i=1; i<=""; i++) {
+var addFizzBuzz = function(number) {
+	for (i=1; i<=number; i++) {
 
 		if (i % 15 == 0) {
 		$("#fizzbuzz").append("<p>FizzBuzz</p>");
@@ -18,15 +18,18 @@ var addFizzBuzz = function() {
 		}
 	}
 }
+var promptUserForInput= function() {
+	var number = prompt("Please Enter Number ");
+	return number; 
+}
+
 
 $(document).ready(function() {
-			var number = prompt("Please Enter Number ");
-			 if(isNaN(number)){
-			 	alert("You must enter a number!");
-			 }
-			else{
-				addFizzBuzz();
-			 }
+	var number = promptUserForInput();
+	while(isNaN(number)){
+		number = promptUserForInput();
+	}
+	addFizzBuzz(number)
 });
 
 
